b819e7bca36010ded17629d1083180ee3d0db458
Bulat Sharipov <SynapDrake@yandex.ru>
2024-10-13 20:55:05 +0500

    Update lib.rs

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -1,3 +1,8 @@
+#[tauri::command]
+fn my_custom_command() -> String {
+  "Hello, world!".into()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -11,6 +16,7 @@ pub fn run() {
             }
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
