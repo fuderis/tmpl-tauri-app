99ad2804f797bb6e1b0ff577022659fcff50026c
Bulat Sharipov <SynapDrake@yandex.ru>
2024-10-13 20:53:05 +0500

    Update main.js

--- ui/assets/scripts/js/main.js
+++ ui/assets/scripts/js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+const invoke = window.__TAURI__.core.invoke;  //  DO NOT REMOVE!!
+
+// Invoke the command
+invoke('my_custom_command')
+    .then(res => console.log(res))
+    .catch(e => console.error(e))
