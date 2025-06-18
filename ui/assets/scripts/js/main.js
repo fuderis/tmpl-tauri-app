b2992aac3ad49ae8b86cbe21c1dd07de4a819232
Bulat Sh. <synapdrake@ya.ru>
2025-06-18 21:42:16 +0500

    Improved application structure

--- ui/assets/scripts/js/main.js
+++ ui/assets/scripts/js/main.js
@@ -1,6 +1,12 @@
 const invoke = window.__TAURI__.core.invoke;  // DO NOT REMOVE!!
 
-// Invoke the command
-invoke("hello", { name: "world" })
-    .then(r => console.log(r))
-    .catch(e => console.error(e))
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('button.click-me').addEventListener('click', () => {
+        invoke("hello", { name: "Developer" })
+            .then(msg => {
+                document.querySelector("h1.title").textContent = msg;
+                console.log(msg);
+            })
+            .catch(e => console.error(e))
+    });
+});
